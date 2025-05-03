c0b2716dd4089039b05715f70e2d63839285eed5
bea4dev <bea0224@outlook.jp>
2025-05-03 17:59:45 +0900

    impl SecondMegaStructure.kt

---
+++ src/main/kotlin/com/github/bea4dev/snowDawn/generator/SecondMegaStructure.kt
@@ -0,0 +1,11 @@
+package com.github.bea4dev.snowDawn.generator
+
+import org.bukkit.generator.ChunkGenerator
+import org.bukkit.generator.WorldInfo
+import java.util.Random
+
+class SecondMegaStructure(private val seed: Long) : ChunkGenerator() {
+    override fun generateNoise(worldInfo: WorldInfo, random: Random, chunkX: Int, chunkZ: Int, chunkData: ChunkData) {
+
+    }
+}
